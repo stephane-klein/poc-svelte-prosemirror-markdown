d5bbbb765b6db5839f3c8443517b0dc01789d527
Stéphane Klein <contact@stephane-klein.info>
2023-08-04 10:35:03 +0200

    Integrate CodeMirror for markdown view

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,10 @@
     import { onMount, onDestroy } from 'svelte'
     import MarkdownIt from 'markdown-it';
 
+    import {basicSetup as CodeBasicSetup, EditorView as CodeEditorView} from "codemirror";
+    import {Compartment} from "@codemirror/state";
+    import {markdown} from "@codemirror/lang-markdown";
+
     import {EditorView as ProseEditorView} from "prosemirror-view"
     import {EditorState as ProseEditorState} from "prosemirror-state"
     import {schema as proseMarkdownSchema, defaultMarkdownParser, defaultMarkdownSerializer} from "../prosemirror-markdown/index.ts";
@@ -10,6 +14,11 @@
 
     let proseElement;
     let proseEditorView;
+
+    let codeElement;
+    let codeEditorView;
+    let languageConf = new Compartment;
+
     let viewMode = 'prose';
     let currentEditor = 'prose';
     let markdownContent;
@@ -25,6 +34,18 @@
                 }),
             }
         );
+
+    }
+
+    function createCodeEditor(content) {
+        codeEditorView = new CodeEditorView({
+            parent: codeElement,
+            doc: content,
+            extensions: [
+                CodeBasicSetup,
+                languageConf.of(markdown())
+            ]
+        });
     }
 
     onMount(() => {
@@ -34,11 +55,15 @@ Hello world!
 * item1
 * item2
         `);
+        // createCodeEditor('Hello world');
     });
 
     onDestroy(() => {
         if (proseEditorView) {
             proseEditorView.destroy()
+        }
+        if (codeEditorView) {
+            codeEditorView.destroy();
         }
     })
 
@@ -46,21 +71,27 @@ Hello world!
         currentEditor = 'markdown';
         proseEditorView.destroy();
         markdownContent = defaultMarkdownSerializer.serialize(proseEditorView.state.doc);
+        createCodeEditor(markdownContent);
     } else if ((viewMode == 'prose') && (currentEditor != 'prose')) {
         currentEditor = 'prose';
+        markdownContent = codeEditorView.state.doc.toString();
+        codeEditorView.destroy();
         createProseEditor(markdownContent);
     }
 </script>
+
 
 <div
     style:display={(viewMode == 'prose' ? 'block' : 'none')}
     bind:this={proseElement}
 ></div>
+<div
+    style:display={(viewMode == 'markdown' ? 'block' : 'none')}
+    bind:this={codeElement}
+></div>
 
 {#if viewMode == 'preview'}
-    {@html md.render(defaultMarkdownSerializer.serialize(proseEditorView.state.doc))}
-{:else if viewMode == 'markdown'}
-    <textarea bind:value={markdownContent} style="height: 20em;"></textarea>
+    {@html md.render(markdownContent)}
 {/if}
 
 <p>
